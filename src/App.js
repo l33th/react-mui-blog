61c395234310dce38e8dc7f1243da8c1c7b37aaa
l33th <l33th@protonmail.ch>
2021-11-24 05:37:16 +0200

    react blog setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<div className='App-Header'>
+				<h1>React MUI Blog Setup</h1>
+			</div>
+		</div>
+	);
+}
+
+export default App;
