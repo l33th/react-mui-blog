7b48480930eeaf8ef4bf2483dc8874da89e3e82f
l33th <l33th@protonmail.ch>
2021-11-25 09:29:16 +0200

    account circle and notification icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Divider, IconButton, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Header = () => {
 	return (
@@ -10,6 +12,12 @@ const Header = () => {
 					<MenuIcon />
 				</IconButton>
 				<Typography variant='h5'>Eleet Developer Hackathon</Typography>
+				<IconButton>
+					<NotificationsIcon />
+				</IconButton>
+				<IconButton>
+					<AccountCircleIcon />
+				</IconButton>
 			</Toolbar>
 			<Divider />
 			<Toolbar>Hack the planet.</Toolbar>
