3e8c3a571c00a33938e4fe1634759e5de070553c
l33th <l33th@protonmail.ch>
2021-11-25 09:10:16 +0200

    container blog setup

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
 import './App.css';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import CommentIcon from '@mui/icons-material/Comment';
+import { Container } from '@mui/material';
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<h1>React MUI Blog</h1>
-				<Stack spacing={2} direction='row'>
-					<Button
-						style={{ backgroundColor: 'green' }}
-						variant='contained'
-						size='large'
-						startIcon={<SendIcon />}
-					>
-						Send
-					</Button>
-					<Button
-						style={{ color: 'white', borderColor: 'white' }}
-						variant='outlined'
-						size='large'
-						startIcon={<CommentIcon />}
-					>
-						Comment
-					</Button>
-					<Button
-						style={{ backgroundColor: 'green' }}
-						variant='contained'
-						size='large'
-						startIcon={<ThumbUpIcon />}
-					>
-						Like
-					</Button>
-				</Stack>
-			</header>
-		</div>
+		<>
+			<Container>
+				<h1>React Blog</h1>
+			</Container>
+		</>
 	);
 }
 
