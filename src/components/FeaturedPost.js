a20970d6ad1db02f8f3bbe9b2f679ebe8a156748
l33th <l33th@protonmail.ch>
2021-11-26 07:57:09 +0200

    added h5 variant for featured post

--- src/components/FeaturedPost.js
+++ src/components/FeaturedPost.js
@@ -7,6 +7,12 @@ const FeaturedPost = () => {
       <Card>
         <CardContent>
           <Typography>This is a long title for the featured post.</Typography>
+          <Typography variant="h5">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos
+            asperiores omnis unde alias quod? Maxime nisi laborum esse qui
+            dolore dolores quae asperiores sequi perspiciatis nobis, reiciendis
+            accusantium ullam corporis.
+          </Typography>
         </CardContent>
       </Card>
     </>
