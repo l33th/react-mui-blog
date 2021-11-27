399274072ed7aa80e93ca9628020775331d158b5
l33th <l33th@protonmail.ch>
2021-11-27 06:01:50 +0200

    main and sidebar props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import { Container, createTheme, Grid, ThemeProvider } from '@mui/material';
-import { featuredPosts } from './data/Data';
+import { featuredPosts, sidebar } from './data/Data';
 import Header from './components/Header';
 import FeaturedPost from './components/FeaturedPost';
 import PostCard from './components/PostCard';
+import Main from './components/Main';
+import Sidebar from './components/Sidebar';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
 
 function App() {
   // dark theme
@@ -12,6 +21,8 @@ function App() {
       mode: 'dark',
     },
   });
+
+  const classes = useStyles();
 
   return (
     <>
@@ -24,6 +35,15 @@ function App() {
             {featuredPosts.map(post => (
               <PostCard post={post} key={post.title} />
             ))}
+          </Grid>
+          <Grid container spacing={5} className={classes.mainGrid}>
+            <Main title="From the Firehouse" />
+            <Sidebar
+              title={sidebar.title}
+              description={sidebar.description}
+              archives={sidebar.archives}
+              social={sidebar.social}
+            />
           </Grid>
         </Container>
       </ThemeProvider>
